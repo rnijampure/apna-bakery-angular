bf9201d5bb2b83e8b3def984d39f35768570aa85
rnijampure <rnijampure@users.noreply.github.com>
2024-07-01 09:32:40 +0530

    Delete apna-bakery-angular directory

--- apna-bakery-angular/browser/chunk-NN4TEYKG.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-LIEWF7E4.js";import"./chunk-IS2RAUVC.js";import"./chunk-2HNTEACI.js";import"./chunk-TEJTRKEH.js";import"./chunk-DMZUDF35.js";export{a as HomeComponent};
--- apna-bakery-angular/browser/chunk-O7BLNSSF.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-LPGN2ETV.js";import"./chunk-TEJTRKEH.js";import"./chunk-DMZUDF35.js";export{a as CartComponent};
